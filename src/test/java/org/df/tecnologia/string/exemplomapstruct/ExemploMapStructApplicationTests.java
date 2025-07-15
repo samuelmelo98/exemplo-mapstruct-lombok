6a03f1849f8c36424021f96d110069d67c714325
Mac_os <samuelmelo@MacBook-Pro-de-Mac-os.local>
2025-07-15 08:13:52 -0300

    Ïnicio do projeto

---
+++ src/test/java/org/df/tecnologia/string/exemplomapstruct/ExemploMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package org.df.tecnologia.string.exemplomapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMapStructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
